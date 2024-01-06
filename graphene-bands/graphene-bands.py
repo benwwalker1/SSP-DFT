050d04eece114ee30aac6a5abc67640ed20d05d8
Ben Walker <wwb00wwb00@gmail.com>
2024-01-06 13:21:20 +0100

    updated graphene band diagram

--- graphene-bands/graphene-bands.py
+++ graphene-bands/graphene-bands.py
@@ -2,6 +2,24 @@ import numpy as np
 import subprocess
 import matplotlib.pyplot as plt
 
+path = [
+    [0, 0, 0, 20],              # GAMMA
+    [0.66666666, -0.33333333, -0.500000, 20],   # K
+    [0.5, -0.5, -0.5, 20],      # M
+    [0, 0, 0, 20]               # GAMMA
+]
+distances = []
+for i in range(len(path) - 1):
+    x1, y1, z1, _ = path[i]
+    x2, y2, z2, _ = path[i + 1]
+    distance = ((x2 - x1) ** 2 + (y2 - y1) ** 2 + (z2 - z1) ** 2) ** 0.5
+    distances.append(distance)
+
+normalized_distances = [int(40*(distance / max(distances))) for distance in distances]
+normalized_distances[1] = 20
+successive_distances_sum = [0] + [sum(normalized_distances[:i+1]) for i in range(len(distances))]
+
+
 # Get scf calculation, save e-density to the tmp folder
 pw_command = "pw.x < graphene-scf.in > graphene-scf.out"
 subprocess.run(pw_command, shell=True)
@@ -37,21 +55,20 @@ if series:
 for series in data:
     k_vector, energy = zip(*series)
     plt.plot(range(len(k_vector)), energy)
-    count = int(np.floor(len(k_vector)/20))
 
 #plt.xlabel("k-vector")
 plt.ylabel("Energy (eV)")
-plt.title("Data Plot")
 
-markers = ['K','\u03B3','M','K']
+markers = ['$\Gamma$','K','M','$\Gamma$']
 # Add text and vertical lines for every 20 data points
 
-for i in range(count+1):
-    text_position = 20*i
+for i in range(len(markers)):
+    text_position = successive_distances_sum[i]
     text_label = markers[i]
     plt.text(text_position, -23.5, text_label, ha='center')
     plt.axvline(x=text_position, color='gray', linestyle='--')
 
 plt.xticks([])  # Disable x-axis number markers
+plt.xlim(0, max(successive_distances_sum))  # Set the x-axis limits
 
 plt.show()
